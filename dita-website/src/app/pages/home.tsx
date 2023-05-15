f7764036258c027cd5ef2b23438f15bedb9232dc
Manu Lamo <straussmoabit@gmail.com>
2023-05-15 14:04:40 +0300

    skeleon seup for he fronend... again.

---
+++ dita-website/src/app/pages/home.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+    return (
+       <div className="h-screen w-full p-6">
+        <h1 className="text-center m-auto text-5xl">DITA Website</h1>
+        <p className="text-center m-auto">To edit the contents of the website, edit the files in the ./pages folder</p>
+       </div>
+    )
+}
