a09185eca6e3133ba85b2beb0bda206793b5cabb
Tendwa-T <tirus301@outlook.com>
2023-05-18 17:54:41 +0300

    Feat: node-skeleton

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const dotenv = require('dotenv').config();
+const cors = require('cors');
+const colors = require('colors');
+
+const connectDB = require('./config/db');
+const eventRouter = require('./routes/eventRoutes');
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+connectDB();
+
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => res.send('API Working'));
+app.use("/api/v1/events", eventRouter);
+
+app.listen(PORT, () => {console.log(`Server is running on port ${PORT}`.red)});
