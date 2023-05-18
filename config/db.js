204ea61e417b081322057cdd5183be5de61ae010
Tirus Khamasi <107058023+Tendwa-T@users.noreply.github.com>
2023-05-18 18:43:31 +0300

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,12 +1,12 @@
 const mongoose = require("mongoose");
-
+const uri =process.env.MONGO_URI;
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI);
+    const conn = await mongoose.connect(uri);
     console.log(`MONGODB connected: ${conn.connection.host}`.cyan.underline);
   } catch (error) {
     console.log(error);
-    process.exit(1);
+    process.exit(0);
   }
 };
 
