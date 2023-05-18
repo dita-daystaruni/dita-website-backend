2b6363cb889df8d64f4ee6376234aff805af342e
Manu Lamo <straussmoabit@gmail.com>
2023-05-18 21:43:36 +0300

    adding a few extra node modules

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,9 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+  mode: 'jit',
   content: [
     "./index.html",
     "./src/**/*.{html,js,jsx}"
   ],
+  darkMode: 'class', 
   theme: {
     extend: {},
   },
