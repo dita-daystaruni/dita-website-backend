b536479880a424f8f0206eac1462630a5b0540ea
Tendwa-T <tirus301@outlook.com>
2023-06-18 17:10:33 +0300

    back-end feature implementation

--- controllers/eventController.js
+++ controllers/eventController.js
@@ -20,17 +20,59 @@ const createEvent = async (req, res) => {
         });
         res.status(201).json({ message: 'Event created successfully', event });
     } catch (error) {
-        res.status(500).json({ message: 'Something went wrong' });
+        res.status(500).json({ message: "Event couldn't be created" });
     }
 };
 
-    const getAllEvents = async (req, res) => {
+const getAllEvents = async (req, res) => {
         try {
             const event = await Event.find().sort({$natural:-1});
             res.status(200).json({ event });
         } catch (error) {
-            res.status(500).json({ message: 'Something went wrong' });
+            res.status(500).json({ message: "Couldn't fetch events" });
         }
 };
 
-module.exports = {createEvent,  getAllEvents}
+const getEvent = async (req, res) => {
+    try {
+        const event = await Event.find({ _id: req.params.id });
+        res.status(200).json({ event });
+    } catch (error) {
+        res.status(500).json({ message: "Couldn't find event" });
+    }
+};
+
+const deleteEvent = async (req, res) => {
+    try {
+        const event = await Event.find({ _id: req.params.id });
+        if (!event) {
+            return res.status(404).json({ message: 'Event not found' });
+        }
+        await Event.findByIdAndDelete({ _id: req.params.id });
+        res.status(200).json({ message: 'Event deleted successfully' });
+    } catch (error) {
+        res.status(500).json({ message: "Couldn't delete event" });
+    }
+};
+
+const updateEvent = async (req, res) => {
+    try {
+        const event = await Event.find({ _id: req.params.id });
+        if (!event) {
+            return res.status(404).json({ message: 'Event not found' });
+        }
+        const updatedEvent = await Event.findByIdAndUpdate(req.params.id,req.body, {new:true,});
+        res.status(200).json({ message: 'Event updated successfully', updatedEvent });
+    } catch (error) {
+        res.status(error.status).json({message: "Couldn't delete event" }); // 500
+    }
+};
+
+
+module.exports = {  
+    createEvent,
+    getAllEvents,
+    getEvent,
+    deleteEvent,
+    updateEvent,
+};
