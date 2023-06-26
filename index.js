3b9c7b1cf40371264fe85f4039b95858f0741414
Tendwa-T <tirus301@outlook.com>
2023-06-26 14:12:40 +0300

    cors config

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Headers', 'Origin, Content-Type, X-Auth-Token');
     next();
 });
-app.use(cors());
+/* app.use(cors()); */
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
