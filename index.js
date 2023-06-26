ae9b1f415f8cad6b400b0277692ec579f0df56d5
Tendwa-T <tirus301@outlook.com>
2023-06-26 11:36:16 +0300

    cors config

--- index.js
+++ index.js
@@ -20,7 +20,13 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 //middleware
-app.use(cors());
+app.use(cors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type'],
+    credentials: true,
+    optionsSuccessStatus: 200
+}));
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
