b92e6a351694a74a9b380b743a445c471173f814
Tendwa-T <tirus301@outlook.com>
2023-07-14 13:34:57 +0300

    user controller update

--- controllers/userController.js
+++ controllers/userController.js
@@ -21,6 +21,7 @@ const createUser = async (req, res) => {
             password: hashedPassword,
             admissionNumber,
         });
+        delete user.password;
         res.status(201).json({ message: 'User created successfully', user});
     } catch (error) {
         res.status(500).json({ message: "User couldn't be created" });
--- models/userModel.js
+++ models/userModel.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const userSchema = mongoose.Schema(
     {
         username: {type: String, required: true},
-        admissionNumber: {type: String, unique:[true,"Admission Number alreadey used"], required: true},
+        admissionNumber: {type: String, unique:[true,"Admission Number already used"], required: true},
         password: {type: String, required: true, select: false},
     },
     {timestamps: true}
