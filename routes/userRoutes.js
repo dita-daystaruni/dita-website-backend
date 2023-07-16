4c4f01f61a99f8d8f9702f76b03c77f6be686462
Tendwa-T <tirus301@outlook.com>
2023-07-16 19:48:43 +0300

    auth1

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -10,8 +10,8 @@ const { checkUser } = require('../middleware/authMiddleware');
 
 const router = express.Router();
 
-router.get("*", checkUser);
-router.post("/create", checkUser ,createUser);
+/* router.get("*", checkUser); */
+router.post("/create" ,createUser);
 router.post("/login", loginUser);
 router.get("/all", getAllUsers);
 router.put("/:id", checkUser, updateUser);
